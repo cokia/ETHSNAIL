f68f064b1f06562431af69db4649d599d57749ab
h4nuko0n <h4nuko0n@h4nuko0nui-MacBookPro.local>
2019-01-13 10:53:26 +0900

    edit sol

--- ETHsnail.sol.go
+++ ETHsnail.sol.go
@@ -1,44 +1,44 @@
 pragma solidity ^0.4.24;
 contract Gamble {
     uint user = 0;
-    uint userbalance = 0;
-    uint random = uint(block.blockhash(block.number-1))%3 + 1;
-    uint Snail1 = 0;
-    uint Snail2 = 0;
-    uint Snail3 = 0;
-    uint winner1 = 0;
-    uint winner2 = 0;
-    uint winner3 = 0;
-    address private owner = msg.sender;
+    uint userbalance = 1;
+    uint winner = 0;
+   // uint random = 7;
+    address public owner = msg.sender;
+    
     function recv (uint A) public {
         user = A;
     }
-    function plus() public payable {
-        userbalance += msg.value;
-    }
+    
     function getBalance() constant public returns(uint) {
         return userbalance;
     }
     
-    function Gamble() public {
-         while(Snail1 >= 20 && Snail2 >= 20 && Snail3 >= 20){
-            uint random1 = uint(block.blockhash(block.number-1))%3 + 1;
-            
-=           if(random1 % 2 == 0){
-                winner = 1     
+    function gambleeeee() public payable{
+    uint random = uint(block.blockhash(block.number-1))%3 + 1;
+          if(random%2==0){
+                winner = 1 ;    
             }
-             if(random1 % 3 == 0){
-                winner = 2    
+             if(random % 3 == 0){
+                winner = 2  ;  
             }
-             if(random1 % 7 == 0){
-                winner = 3   
-            }
+             if(random % 7 == 0){
+                winner = 3  ; 
             }
          }
-        } 
+    function getresult() constant public returns(uint) {
+        return winner;
+    }
     
-    function reward(uint ub) public payable{   
-        msg.sender.send(ub);
+    function reward() public payable{
+        
+        if(user == winner){
+            userbalance += 2;
+        }
+         if(user != winner){
+            userbalance -= 1;
+        }
+        //owner.transfer(userbalance);
         }
 }
     
